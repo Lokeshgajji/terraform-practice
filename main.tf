d860e87aa9881fb3bb0ee1c85d08c7f4fdc06b74
Lokeshgajji <132226574+Lokeshgajji@users.noreply.github.com>
2024-02-14 16:26:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-variable "ami_value" {
-  description = "get ami"
-  }
-variable "instance_type_value" {
-  description = "get instance"
-  }
- variable "subnet_id_value" {
-  description ="get subniet id"
-  }
 provider "aws" {
   region = "us-east-1"
   }
