14ab01e87e35760f828510319597074d3c20b1b1
Lokeshgajji <132226574+Lokeshgajji@users.noreply.github.com>
2024-02-14 16:26:00 -0500

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+  description = "get ami"
+  }
+variable "instance_type_value" {
+  description = "get instance"
+  }
+ variable "subnet_id_value" {
+  description ="get subniet id"
+  }
